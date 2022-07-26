c06758410e176ccf642984a57db9e86644f8a557
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-07-26 15:15:59 +0200

    imported icon

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    
+    <MainHeader />
   </div>
 </template>
 
 <script>
+import MainHeader from './components/MainHeader.vue';
 
 export default {
   name: 'App',
   components: {
-  }
+    MainHeader
+}
 }
 </script>
 
 <style lang="scss">
-@import './assets/sass/style.scss'
+@import './assets/sass/style.scss';
+
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav class="nav p-2">
+        <img src="../assets/img/spotify.png" alt="spotify logo">
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/sass/vars';
+    .nav{
+        height: 70px;
+        background-color: $primary_color;
+
+        img{
+            height: 50px;
+            width: 50px;
+        }
+    }
+</style>
